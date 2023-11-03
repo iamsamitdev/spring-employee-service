8afe00d1ff2d0bbde15ce3371fd458482760c1f0
Samit Koyom <samitkoyom@gmail.com>
2023-11-03 15:46:24 +0700

    update app

--- src/main/java/com/itgenius/employeeservice/EmployeeServiceApplication.java
+++ src/main/java/com/itgenius/employeeservice/EmployeeServiceApplication.java
@@ -2,9 +2,9 @@ package com.itgenius.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+// import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient // ประกาศให้เป็น Discovery Client
+// @EnableDiscoveryClient // ประกาศให้เป็น Discovery Client
 @SpringBootApplication
 public class EmployeeServiceApplication {
 
